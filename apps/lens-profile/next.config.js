3f28a006c2ca1cd48a8d724fe2eb0419efde2970
koons-koou <koons-koou@gmail.com>
2023-02-15 17:07:06 +0800

    change runtime runtime

--- apps/lens-profile/next.config.js
+++ apps/lens-profile/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    runtime: "experimental-edge",
+  },
   images: {
     domains: ['lens.infura-ipfs.io'],
     unoptimized: true,
