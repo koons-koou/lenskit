2959f5510667f090279c38536bbcf105d4dd0514
koons-koou <koons-koou@gmail.com>
2023-02-16 17:24:11 +0800

    new Level and add share

--- apps/lens-profile/pages/[handle].tsx
+++ apps/lens-profile/pages/[handle].tsx
@@ -5,6 +5,15 @@ import LensCalendar from '../components/LensCalendar'
 import Placeholder from '../components/Placeholder'
 import ProfileStats from '../components/ProfileStats'
 import { fetchDataPoints } from '../lib/datapoints'
+import {
+  ShareIcon,
+} from '@heroicons/react/24/solid'
+
+import { useRef, useState } from 'react'
+// @ts-ignore
+import html2canvas from 'html2canvas';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Profile({
   initHandle,
@@ -12,9 +21,11 @@ export default function Profile({
   const router = useRouter()
   const handle = initHandle ?? router.query.handle?.toString()
   const realhandle =
-    handle == 'lensprotocol' ? handle : handle?.endsWith('.lens') ? handle : handle + '.lens'
+    handle == 'lensprotocol' ? handle : handle?.endsWith('.lens') ? handle : handle + '.lens';
+  const [saveLoading,setSaveLoading] = useState(false);
   const { profiles, loading, error } = useProfiles({ handles: [realhandle] })
-  const profileId = profiles && profiles.length > 0 && profiles[0].id
+  const profileId = profiles && profiles.length > 0 && profiles[0].id;
+  const ref = useRef(null);
 
   const { data: data2022 } = useSWR(profileId ? [profileId, 2022] : null, ([profileId, year]) =>
     fetchDataPoints(profileId, year)
@@ -43,18 +54,56 @@ export default function Profile({
     )
   }
 
+  const downloadImage = ()=>{
+    if(ref.current && typeof window !== 'undefined'){
+      setSaveLoading(true)
+      html2canvas(ref.current,{
+        dpi: window.devicePixelRatio * 2, // 对应屏幕的dpi，适配高清屏，解决canvas模糊问题
+        scale: 2, // 缩放
+        allowTaint: true, // 是否使用图片跨域
+        useCORS: true, // 是否使用图片跨域
+        backgroundColor: null,
+      }).then((canvas: any) => {
+        const save_url = canvas.toDataURL("image/png");
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.href = save_url;
+        a.target = '_blank'
+        a.download = `${realhandle}.png`;
+        a.click();
+        setSaveLoading(false);
+      }).catch(()=>setSaveLoading(false));
+    }
+  }
+
   return (
-    <div className="mx-auto md:my-12 md:mt-12 max-w-screen-xl w-full h-full"> 
+    <div className="mx-auto md:my-12 md:mt-12 max-w-screen-xl w-full h-full relative" > 
       <div
         className={
-          `mx-auto max-w-screen-md flex flex-col items-center space-y-10 rounded-2xl p-6 md:p-12 shadow-2xl md:px-3 ` +
+          `mx-auto w-full max-w-screen-md flex flex-col items-center justify-start pt-12 pb-4 px-3 rounded-2xl ` +
           randomGradient()
         }
+        ref={ref}
       >
-        <ProfileStats handle={realhandle} />
+        <ProfileStats handle={realhandle} datapoints={[...data2022,...data2023]}/>
         {data2023 && <LensCalendar profileId={profileId} year={2023} datapoints={data2023} />}
         {data2022 && <LensCalendar profileId={profileId} year={2022} datapoints={data2022} />}
+        <div className="mt-4 px-8 w-full flex items-center justify-between text-white text-left">
+          <Link href={window.location.href} target="_blank" className="hover:text-teal-800">{window.location.href}</Link>
+          <Image
+                  className="rounded-full"
+                  src={'/lens-logo-china.png'}
+                  alt=""
+                  width={200}
+                  height={62}
+                  placeholder="blur"
+                  blurDataURL="/lens-logo-china.png"
+                />
+        </div>
       </div>
+      <button disabled={saveLoading} onClick={()=>downloadImage()} className="absolute right-0 md:right-10 top-20 mt-10 rounded-full bg-gray-100 text-teal-600 shadow-xl cursor-pointer p-2 w-12 h-12 flex items-center justify-center text-center hover:rotate-12 hover:text-green-700">
+        {saveLoading ? '截图中...' : <ShareIcon className="w-8 h-8"/> }
+      </button>
     </div>
   )
 }
